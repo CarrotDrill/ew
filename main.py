2998403200b760b4c0347c1c290dcde820679fb5
CarrotDrill <86727101+CarrotDrill@users.noreply.github.com>
2021-07-08 15:49:58 +0000

    turtle changes

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import turtle
 
-turtle.bgcolor("black")
-turtle.setup(600,600)
+
 screen = turtle.Screen()
 
 t = turtle.Turtle()
 t.color("red")
 t.shape("turtle")
 t.pensize(5)
+turtle.bgcolor("black")
 
 def circle():
-  t.circle(100)
+  t.circle(150)
 
 x = 0 
 while x > 30:
   circle()
   x += 1
+
