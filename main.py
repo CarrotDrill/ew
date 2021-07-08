fa1cde93c64e8b2fe7cc5f8e6dcd20d142685dfd
CarrotDrill <86727101+CarrotDrill@users.noreply.github.com>
2021-07-08 16:01:42 +0000

    another turtle

--- main.py
+++ main.py
@@ -9,11 +9,27 @@ t.shape("turtle")
 t.pensize(5)
 turtle.bgcolor("black")
 
+t2 = turtle.Turtle()
+t.color("green")
+t.shape("arrow")
+t.pensize(3)
+
+def square():
+  for i in range(4)
+    t2.forward(100)
+    t2.left(90)
+
 def circle():
   t.circle(150)
 
+
+
 x = 0 
-while x > 30:
+while x < 30:
   circle()
+  t.right(10)
+  square()
+  t2.left(15)
   x += 1
+
 
